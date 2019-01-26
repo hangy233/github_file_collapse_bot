453f8206f8335599d601f6cafc5b944774735446
Hang Ye <hangy@yahoo-inc.com>
2019-01-25 16:44:28 -0800

    save typed value

--- src/main.js
+++ src/main.js
@@ -11,13 +11,25 @@ document.addEventListener('DOMContentLoaded', () => {
   const keywordInput = document.getElementById('keyword-input');
   const isRegexCheckbox = document.getElementById('regex-enabled');
 
-  container.addEventListener('click', ({target}) => {
-    const payload = {
-      keyword: keywordInput.value,
-      isCollapse: target.id === 'collapse-btn',
-      isRegex: isRegexCheckbox.checked
-    };
+  chrome.storage.local.get(['keyword'], function(result) {
+    if (result.keyword) {
+      keywordInput.value = result.keyword;
+    }
 
-    postMessage(payload);
+    container.addEventListener('click', ({target}) => {
+      if (!target.classList.contains('button')) {
+        return;
+      }
+
+      const payload = {
+        keyword: keywordInput.value,
+        isCollapse: target.id === 'collapse-btn',
+        isRegex: isRegexCheckbox.checked
+      };
+
+      chrome.storage.local.set({keyword: keywordInput.value});
+
+      postMessage(payload);
+    });
   });
 });
